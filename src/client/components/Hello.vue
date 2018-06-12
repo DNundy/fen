0035739e4571e99acb8e31672b2ca72bb7a12230
Nundy <18710939328@163.com>
2018-06-12 16:05:30 +0800

    init

---
+++ src/client/components/Hello.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hellos World!'
+    }
+  }
+}
+</script>
+<style lang="less">
+*{
+  color: red;
+}
+</style>
+
